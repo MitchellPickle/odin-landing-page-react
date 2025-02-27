fec58eda0c85081a35732045e58eb4219f733d18
MitchellPickle <94079796+MitchellPickle@users.noreply.github.com>
2025-02-27 21:17:06 +0000

    Added some resources from other repository 'odin-landing-page'

--- landing-page/src/App.js
+++ landing-page/src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './styles.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <html lang="en">
+      <head>
+          <meta charSet="UTF-8"/>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+          <title>Landing Page</title>
+          <link rel='stylesheet' href="styles.css"/>
+      </head>
+      <body>
+          <div className="container">
+            <header>
+                <div className="header">
+                    <div id="header-image">
+                        <img src="photos/Pizza.svg" alt="Pizza"/>
+                    </div>
+
+                    <div className="link">
+                        <ul>
+                          <li><a href="https://www.google.com" target="_blank">Google</a></li>
+                          <li><a href="https://www.youtube.com/watch?v=_ZJ7AAoDhu4" target="_blank">YouTube</a></li>
+                          <li><a href="https://en.wikipedia.org/wiki/Pizza" target="_blank">Wikipedia</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </header>
+          </div>
+      </body>
+    </html>
   );
 }
 
